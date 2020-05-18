ca62de843ef6f423fa531f553f69637d8f94493c
Luka729 <58185216+Luka729@users.noreply.github.com>
2020-05-19 01:11:48 +0200

    Dodavanje baze podataka

---
+++ WebApplication1/WebApplication1/Migrations/20200518230151_first.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.FirstName);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
