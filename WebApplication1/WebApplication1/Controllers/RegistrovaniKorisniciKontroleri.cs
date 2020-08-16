89a025b8dee9c3a614f3fe34bae0ac9079e8827e
kristinag-maker <58185195+kristinag-maker@users.noreply.github.com>
2020-08-16 21:16:03 +0200

    Dodata logika za registrovanje korisnika

---
+++ WebApplication1/WebApplication1/Controllers/RegistrovaniKorisniciKontroleri.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+using WebApplication1.Klase;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrovaniKorisniciKontroleri : ControllerBase
+    {
+        private readonly MyDbContext _context;
+        private readonly UserManager<RegistrovaniKorisniciKlasa> userManager;
+
+        public RegistrovaniKorisniciKontroleri(MyDbContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        [Route("Registrovanje")]
+        public async Task<ActionResult<RegistrovaniKorisniciKlasa>> DodajKorisnika(RegistrovaniKorisniciKlasa registrovaniKorisnici)
+        {
+            if (await userManager.FindByEmailAsync(registrovaniKorisnici.eadresaProvera) != null){
+                return BadRequest(new { message = "Vec postoji korisnik sa takvom e-mail adresom" });
+            }
+            try
+            {
+                var rezultat = userManager.CreateAsync(registrovaniKorisnici, registrovaniKorisnici.lozinkaProvera);
+                return Ok(rezultat);
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+
+    }
+}
--- WebApplication1/WebApplication1/MyDbContext.cs
+++ WebApplication1/WebApplication1/MyDbContext.cs
@@ -1,16 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.Klase;
 
 namespace WebApplication1
 {
-    public class MyDbContext : DbContext
+    public class MyDbContext :  IdentityDbContext
     {
         public MyDbContext(DbContextOptions<MyDbContext> options)
-: base(options)
+: base(options) 
         {
-        }       
+
+    }
+        public DbSet<RegistrovaniKorisniciKlasa> RegistrovaniKorisnici { get; set; }
+
     }
 }
